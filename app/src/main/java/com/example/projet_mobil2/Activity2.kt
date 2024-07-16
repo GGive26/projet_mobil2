78c92976ab116d2d05ba407163fedc1ba6c59a23
GGive26 <precieudieudonne@gmail>
2024-07-16 17:52:47 -0400

    avancer sans recuprer les evenements actuels

--- app/src/main/java/com/example/projet_mobil2/Activity2.kt
+++ app/src/main/java/com/example/projet_mobil2/Activity2.kt
@@ -1,8 +1,11 @@
 package com.example.projet_mobil2
 
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -24,6 +27,7 @@ class Activity2 : AppCompatActivity() {
     private lateinit var latitude:EditText
     private lateinit var adresse:EditText
     private lateinit var date_stockage:EditText
+    private lateinit var confirmerButton :Button
 
     private var id=-1
     private val calendar = Calendar.getInstance()
@@ -48,6 +52,7 @@ class Activity2 : AppCompatActivity() {
         humidite_min = findViewById(R.id.edit_hum_min)
         temperature_max = findViewById(R.id.edit_temp_max)
         temperature_min = findViewById(R.id.edit_temp_min)
+        confirmerButton=findViewById(R.id.btn_confirmation)
 
         // Gestion du sélecteur de date
         val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
@@ -65,52 +70,77 @@ class Activity2 : AppCompatActivity() {
                 calendar.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
+        confirmerButton.setOnClickListener {
+            val nom = nom_entrepot.text.toString()
+            val type = type_entrepot.text.toString()
+            val date_stock = date_stockage.text.toString()
+            val tempMax = temperature_max.text.toString()
+            val tempMin = temperature_min.text.toString()
+            val humMax = humidite_max.text.toString()
+            val humMin = humidite_min.text.toString()
+            val long = longitude.text.toString()
+            val lat = latitude.text.toString()
+
+            if (verifierFormulaire(nom, type, date_stock, tempMax,tempMin,humMax,humMin,long,lat)) {
+                // Préparation du résultat pour retourner à MainActivity
+                val resultIntent = Intent()
+                resultIntent.putExtra("id", id)
+                resultIntent.putExtra("type", type)
+                resultIntent.putExtra("temperature_max", tempMax)
+                resultIntent.putExtra("temperature_min", tempMin)
+                resultIntent.putExtra("humidite_max", humMax)
+                resultIntent.putExtra("humidite_min", humMin)
+               // resultIntent.putExtra("temperature_act")
+                //resultIntent.putExtra("humidite_act", auteur)
+                //resultIntent.putExtra("adresse", datePublication)
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish()
+            }
+        }
 
     }
-    private fun verifierFormulaire(nom: String, type: String, date_stockage: String, temp_max: String,temp_min:Int,hum_max:Int,hum_min:Int,long:String,lat:String): Boolean {
-        // Vérification du titre
+    private fun verifierFormulaire(nom: String, type: String, date_stock: String, tempMax: String,tempMin: String,humMax: String,humMin: String,long: String,lat: String): Boolean {
+        // Vérification du nom
         if (nom.isEmpty()) {
             nom_entrepot.error = "Le nom de l'entrepot doit être non vide"
             return false
         }
 
-        // Vérification de l'auteur
+        // Vérification du type
         if (type.isEmpty()) {
             type_entrepot.error = "le type  doit être non vide"
             return false
         }
 
-        // Vérification de la date de publication
-        if (date_stockage.isEmpty() || !date_stockage.matches(Regex("\\d{4}-\\d{2}-\\d{2}"))) {
-            date_stockage.error = "Date de publication invalide"
+        // Vérification de la date de stockage
+        if (date_stock.isEmpty() || !date_stock.matches(Regex("\\d{4}-\\d{2}-\\d{2}"))) {
             return false
         }
 
-        // Vérification du numéro ISBN
-        if (temp_max.isEmpty() || temp_max.length != 13) {
-            temp_max.error = "La temperature max est invalide"
+        // Vérification de la temperature max
+        if (tempMax.isEmpty() || tempMax.length != 13) {
             return false
         }
-        // Vérification du titre
-        if (temp_min.isEmpty()) {
-            temp_min.error = "La temperature min est invalide"
-            return false
-        }
-
-        // Vérification de l'auteur
-        if (type.isEmpty()) {
-            type_entrepot.error = "le type  doit être non vide"
-            return false
-        }
-        // Vérification du titre
-        if (nom.isEmpty()) {
-            nom_entrepot.error = "Le nom de l'entrepot doit être non vide"
+        // Vérification de la temperature minimale
+        if (tempMin.isEmpty()) {
             return false
         }
 
-        // Vérification de l'auteur
-        if (type.isEmpty()) {
-            type_entrepot.error = "le type  doit être non vide"
+        // Vérification de la humidite min
+        if (humMin.isEmpty()) {
+            return false
+        }
+        // Vérification de la humidite max
+        if (humMax.isEmpty()) {
+            return false
+        }
+
+        // Vérification de la longitude
+        if (long.isEmpty()) {
+            return false
+        }
+        // Vérification de la latitude
+        if (lat.isEmpty()) {
             return false
         }
 
