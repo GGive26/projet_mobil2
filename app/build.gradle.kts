d66b8c39813daf462b29aa53b5648512bce034a8
GGive26 <precieudieudonne@gmail>
2024-07-30 23:19:11 -0400

    firebase connecter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,10 +36,20 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/projet_mobil2/Activity3.kt
+++ app/src/main/java/com/example/projet_mobil2/Activity3.kt
@@ -5,10 +5,14 @@ import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.AdapterView
 import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
 
 class Activity3 : AppCompatActivity() {
 
@@ -18,33 +22,46 @@ class Activity3 : AppCompatActivity() {
     private lateinit var dbHelper: DatabaseHelper
     private val livresList = mutableListOf<Entrepot>()
 
-    private val ajouterEntrepotLauncher= registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
-        if (result.resultCode == RESULT_OK) {
-            val data = result.data
-            if (data != null) {
-                // Récupération des données renvoyées par AjouterLivreActivity
-                var NOM_EMPLACEMENT = data.getStringExtra("nom")
-                var TYPE = data.getStringExtra("type")
-                var DATE_STOCKAGE=data.getStringExtra("date_stockage")
-                var TEMPERATURE_MAX = data.getStringExtra("temperature_max").toString().toInt()
-                var TEMPERATURE_MIN = data.getStringExtra("temperature_min").toString().toInt()
-                var HUMIDITE_MAX = data.getStringExtra("humidite_max").toString().toInt()
-                var HUMIDITE_MINIM = data.getStringExtra("humidite_min").toString().toInt()
+    //declaration des variables de firebase
+    private lateinit var firebaseDatabase: FirebaseDatabase //bd
+    private lateinit var firebaseProductsRef: DatabaseReference
 
-                var entrepot= Entrepot(NOM_EMPLACEMENT!!,TYPE!!, DATE_STOCKAGE!!,TEMPERATURE_MAX,TEMPERATURE_MIN,HUMIDITE_MAX,HUMIDITE_MINIM)
+    private val ajouterEntrepotLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data
+                if (data != null) {
+                    // Récupération des données renvoyées par AjouterLivreActivity
+                    var NOM_EMPLACEMENT = data.getStringExtra("nom")
+                    var TYPE = data.getStringExtra("type")
+                    var DATE_STOCKAGE = data.getStringExtra("date_stockage")
+                    var TEMPERATURE_MAX = data.getStringExtra("temperature_max").toString().toInt()
+                    var TEMPERATURE_MIN = data.getStringExtra("temperature_min").toString().toInt()
+                    var HUMIDITE_MAX = data.getStringExtra("humidite_max").toString().toInt()
+                    var HUMIDITE_MINIM = data.getStringExtra("humidite_min").toString().toInt()
 
-                if (NOM_EMPLACEMENT.isNotEmpty()) {
-                    ajouterEntrepot(entrepot)
-                } else {
-                    mettreAJourEntrepot(entrepot)
-                }
-                // Notification à l'adapter que les données ont changé
-                runOnUiThread {
-                    entrepotAdapter.notifyDataSetChanged()
+                    var entrepot = Entrepot(
+                        NOM_EMPLACEMENT!!,
+                        TYPE!!,
+                        DATE_STOCKAGE!!,
+                        TEMPERATURE_MAX,
+                        TEMPERATURE_MIN,
+                        HUMIDITE_MAX,
+                        HUMIDITE_MINIM
+                    )
+
+                    if (NOM_EMPLACEMENT.isNotEmpty()) {
+                        ajouterEntrepot(entrepot)
+                    } else {
+                        mettreAJourEntrepot(entrepot)
+                    }
+                    // Notification à l'adapter que les données ont changé
+                    runOnUiThread {
+                        entrepotAdapter.notifyDataSetChanged()
+                    }
                 }
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,28 +70,30 @@ class Activity3 : AppCompatActivity() {
         dbHelper = DatabaseHelper(this)
 
 
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseProductsRef = firebaseDatabase.getReference("Project-Mobil") //name??
         // Initialisation des vues
         entrepotListView = findViewById(R.id.list_item)
         ajouterEntrepotButton = findViewById(R.id.btnAjouter)
         dbHelper = DatabaseHelper(this)
 
 
-        val nom:String=intent.getStringExtra("nom").toString()
-        val type:String=intent.getStringExtra("type").toString()
-        val date_s:String=intent.getStringExtra("date_stockage").toString()
-        val temp_max =intent.getStringExtra("temperature_max").toString().toInt()
-        val temp_min=intent.getStringExtra("temperature_min").toString().toInt()
-        val hum_max=intent.getStringExtra("humidite_max").toString().toInt()
-        val hum_min=intent.getStringExtra("humidite_min").toString().toInt()
+        val nom: String = intent.getStringExtra("nom").toString()
+        val type: String = intent.getStringExtra("type").toString()
+        val date_s: String = intent.getStringExtra("date_stockage").toString()
+        val temp_max = intent.getStringExtra("temperature_max").toString().toInt()
+        val temp_min = intent.getStringExtra("temperature_min").toString().toInt()
+        val hum_max = intent.getStringExtra("humidite_max").toString().toInt()
+        val hum_min = intent.getStringExtra("humidite_min").toString().toInt()
 
-        val item= Entrepot(nom,type,date_s,temp_max,temp_min,hum_max,hum_min)
+        val item = Entrepot(nom, type, date_s, temp_max, temp_min, hum_max, hum_min)
 
         livresList.add(item)
         ajouterEntrepot(item)
 
         // Configuration de l'adapter pour la ListView
-         entrepotAdapter = EntrepotAdapter(this@Activity3, livresList)
-         entrepotListView.adapter = entrepotAdapter
+        entrepotAdapter = EntrepotAdapter(this@Activity3, livresList)
+        entrepotListView.adapter = entrepotAdapter
 
 
         // Chargement des livres depuis la base de données local
@@ -86,51 +105,73 @@ class Activity3 : AppCompatActivity() {
         }
 
         // Gestion du clic sur un item de la liste pour éditer le livre
-        entrepotListView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val e = livresList[position]
-            val intent = Intent(this, Activity2::class.java)
-            intent.putExtra("type", e.TYPE)
-            intent.putExtra("nom",e.NOM_EMPLACEMENT)
-            intent.putExtra("date_s",e.DATE_STOCKAGE)
-            intent.putExtra("temperature_max", e.TEMPERATURE_MAX)
-            intent.putExtra("temperature_min", e.TEMPERATURE_MIN)
-            intent.putExtra("humidite_max", e.HUMIDITE_MAX)
-            intent.putExtra("humidite_min", e.HUMIDITE_MIN)
-            ajouterEntrepotLauncher.launch(intent)
-        }
+        entrepotListView.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val e = livresList[position]
+                val intent = Intent(this, Activity2::class.java)
+                intent.putExtra("type", e.TYPE)
+                intent.putExtra("nom", e.NOM_EMPLACEMENT)
+                intent.putExtra("date_s", e.DATE_STOCKAGE)
+                intent.putExtra("temperature_max", e.TEMPERATURE_MAX)
+                intent.putExtra("temperature_min", e.TEMPERATURE_MIN)
+                intent.putExtra("humidite_max", e.HUMIDITE_MAX)
+                intent.putExtra("humidite_min", e.HUMIDITE_MIN)
+                ajouterEntrepotLauncher.launch(intent)
+            }
 
         // Gestion du long clic sur un item de la liste pour supprimer le livre
-        entrepotListView.onItemLongClickListener = AdapterView.OnItemLongClickListener { _, _, position, _ ->
-            val e = livresList[position]
-            AlertDialog.Builder(this)
-                .setTitle("Confirmation")
-                .setMessage("Voulez-vous supprimer ce livre?")
-                .setPositiveButton("Oui") { dialog, _ ->
-                    supprimerEntrepot(e, position)
-                    dialog.dismiss()
-                }
-                .setNegativeButton("Non", null)
-                .show()
-            true
-        }
+        entrepotListView.onItemLongClickListener =
+            AdapterView.OnItemLongClickListener { _, _, position, _ ->
+                val e = livresList[position]
+                AlertDialog.Builder(this)
+                    .setTitle("Confirmation")
+                    .setMessage("Voulez-vous supprimer ce livre?")
+                    .setPositiveButton("Oui") { dialog, _ ->
+                        supprimerEntrepot(e, position)
+                        dialog.dismiss()
+                    }
+                    .setNegativeButton("Non", null)
+                    .show()
+                true
+            }
     }
 
     private fun ajouterEntrepot(entrepot: Entrepot) {
 
-            // Ajouter le livre à la base de données locale
-            val db = dbHelper.writableDatabase
-            val values = ContentValues().apply {
-                put(DatabaseHelper.COLUMN_NOM_EMPLACEMENT, entrepot.NOM_EMPLACEMENT)
-                put(DatabaseHelper.COLUMN_TYPE,entrepot.TYPE)
-                put(DatabaseHelper.COLUMN_DATE_STOCKAGE, entrepot.DATE_STOCKAGE)
-                put(DatabaseHelper.COLUMN_HUMIDITE_MAX, entrepot.HUMIDITE_MAX)
-                put(DatabaseHelper.COLUMN_TEMPERATURE_MAX, entrepot.TEMPERATURE_MAX)
-                put(DatabaseHelper.COLUMN_HUMIDITE_MIN, entrepot.HUMIDITE_MIN)
-                put(DatabaseHelper.COLUMN_TEMPERATURE_MIN, entrepot.TEMPERATURE_MIN)
+        // Ajouter le livre à la base de données locale
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(DatabaseHelper.COLUMN_NOM_EMPLACEMENT, entrepot.NOM_EMPLACEMENT)
+            put(DatabaseHelper.COLUMN_TYPE, entrepot.TYPE)
+            put(DatabaseHelper.COLUMN_DATE_STOCKAGE, entrepot.DATE_STOCKAGE)
+            put(DatabaseHelper.COLUMN_HUMIDITE_MAX, entrepot.HUMIDITE_MAX)
+            put(DatabaseHelper.COLUMN_TEMPERATURE_MAX, entrepot.TEMPERATURE_MAX)
+            put(DatabaseHelper.COLUMN_HUMIDITE_MIN, entrepot.HUMIDITE_MIN)
+            put(DatabaseHelper.COLUMN_TEMPERATURE_MIN, entrepot.TEMPERATURE_MIN)
 
-            }
-            db.insert(DatabaseHelper.TABLE_ENTREPOT, null, values)
         }
+        db.insert(DatabaseHelper.TABLE_ENTREPOT, null, values)
+
+
+//ajouter dans firebase
+        firebaseProductsRef.child(entrepot.NOM_EMPLACEMENT).setValue(entrepot)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Ajouter au Firebase avec succes", Toast.LENGTH_LONG)
+                        .show()
+// Notification à l'adapter que les données ont changé
+                    runOnUiThread {
+                        entrepotAdapter.notifyDataSetChanged()
+                    }
+                } else {
+                    Log.e(
+                        "FirebaseError",
+                        "Erreur en ajoutant un produit dans Firebase",
+                        task.exception
+                    )
+                }
+            }
+    }
 
     // Méthode pour mettre à jour un livre
     private fun mettreAJourEntrepot(entrepot: Entrepot) {
@@ -149,6 +190,22 @@ class Activity3 : AppCompatActivity() {
         val selectionArgs = arrayOf(entrepot.NOM_EMPLACEMENT)
         db.update(DatabaseHelper.TABLE_ENTREPOT, values, selection, selectionArgs)
 
+        firebaseProductsRef.child(entrepot.NOM_EMPLACEMENT).setValue(entrepot)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+// Notification à l'adapter que les données ont changé
+                    runOnUiThread {
+                        entrepotAdapter.notifyDataSetChanged()
+                    }
+                } else {
+                    Log.e(
+                        "FirebaseError",
+                        "Erreur en ajoutant un produit dans Firebase",
+                        task.exception
+                    )
+                }
+            }
+
     }
 // Méthode pour supprimer un livre
     private fun supprimerEntrepot(Entrepot: Entrepot, position: Int) {
@@ -159,6 +216,20 @@ class Activity3 : AppCompatActivity() {
             db.delete(DatabaseHelper.TABLE_ENTREPOT, selection, selectionArgs)
             // Suppression du livre de la liste locale
             livresList.removeAt(position)
+
+            firebaseProductsRef.child(Entrepot.NOM_EMPLACEMENT).removeValue()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //reaffiche les produits restants
+                        chargerEntrepotDepuisLocal()
+                        // Notification à l'adapter que les données ont changé
+                        runOnUiThread {
+                            entrepotAdapter.notifyDataSetChanged()
+                        }
+                    } else {
+                        Log.e("FirebaseError", "Erreur en supprimant un produit dans Firebase", task.exception)
+                    }
+                }
             // Notification à l'adapter que les données ont changé
             runOnUiThread {
                 entrepotAdapter.notifyDataSetChanged()
@@ -183,6 +254,8 @@ class Activity3 : AppCompatActivity() {
 
                 var entrepot = Entrepot(nom_entrepot, type_entrepot, date_stock,temp_max,temp_min,humidite_max,humidite_min)
                 livresList.add(entrepot)
+
+                firebaseProductsRef.child(nom_entrepot.toString()).setValue(entrepot) //already a string ??
             } while (cursor.moveToNext())
         }
         cursor.close()
